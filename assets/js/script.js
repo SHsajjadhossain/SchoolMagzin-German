b0b2b4d706dea24533f3ac626e55bd52b52e38e6
Sajjad Hossain <sajjadhossain.hs@gmail.com>
2024-06-08 19:35:45 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// ==========================
+//   active na js start
+// ==========================
+
+// $(document).ready(function () {
+//     var Item = document.querySelectorAll('.nav-link');
+//     var CurrentLocation = location.href;
+
+//     for(var i = 0; i < Item.length; i++) {
+//         if (Item[i].href == CurrentLocation) {
+//             Item[i].className = "active";
+//         }
+//     }
+// });
+
+// ==========================
+//   active na js end
+// ==========================
