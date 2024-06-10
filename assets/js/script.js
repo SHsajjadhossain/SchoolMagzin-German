4e6781700abe39fe3948fb1b87ba595d945f508e
Sajjad Hossain <sajjadhossain.hs@gmail.com>
2024-06-10 16:59:55 +0600

    working on responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // ==========================
-//   active na js start
+//   active nav js start
 // ==========================
 
 // $(document).ready(function () {
@@ -14,5 +14,23 @@
 // });
 
 // ==========================
-//   active na js end
+//   active nav js end
+// ==========================
+
+// ==========================
+//   mobile nav js start
+// ==========================
+
+$('.bar').click(function (e) { 
+    $('.mobile-nav').fadeToggle();
+    
+});
+
+$('.close-icon').click(function (e) { 
+    $('.mobile-nav').fadeToggle();
+    
+});
+
+// ==========================
+//   mobile nav js end
 // ==========================
